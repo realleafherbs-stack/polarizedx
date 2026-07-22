cfac52a8a3f9c7905c0618f72b2212f77a84daa4
Anton <antonr1wk@gmail.com>
2026-07-22 15:43:25 +0300

    f

--- app/api/hyp-checkout/route.ts
+++ app/api/hyp-checkout/route.ts
@@ -120,6 +120,7 @@ export async function POST(req: NextRequest) {
     PageLang: "HEB",
     sendemail: "True",
     MoreData: "True",
+    tmp: "3",
     SuccessUrl: `${siteUrl}/payment/success`,
     ErrorUrl: `${siteUrl}/payment/failure`,
   });
